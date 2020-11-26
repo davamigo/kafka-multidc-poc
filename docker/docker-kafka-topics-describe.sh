8985c55b58bc160989012dc9c5013860d12b96a1
David Amigo <david.amigo@privalia.com>
2020-11-26 15:51:11 +0100

    Improve some docker scripts to allow choosing the broker where tu run the command

--- docker/docker-kafka-topics-describe.sh
+++ docker/docker-kafka-topics-describe.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 echo -e "\033[33mThis script describes a topic.\033[0m"
 echo -e "\033[32mUsage:\033[0m"
-echo -e "\t$0 TOPIC\n"
+echo -e "\t$0 TOPIC [BROKER]\n"
 
 TOPIC="$1"
 shift
@@ -14,10 +14,17 @@ if [ "$TOPIC" == "" ]; then
   exit
 fi
 
-echo -e "\033[33mDescribing the topic $TOPIC...\033[0m\n"
-echo -e "$ docker exec -t kpoc_broker1 kafka-topics.sh --bootstrap-server=localhost:9092 --describe --topic $TOPIC\n"
+BROKER="$1"
+shift
 
-docker exec -t kpoc_broker1 \
+if [ "$BROKER" == "" ]; then
+  BROKER="kpoc_broker1"
+fi
+
+echo -e "\033[33mDescribing the topic $TOPIC...\033[0m\n"
+echo -e "$ docker exec -t $BROKER kafka-topics.sh --bootstrap-server=localhost:9092 --describe --topic $TOPIC\n"
+
+docker exec -t $BROKER \
   kafka-topics.sh \
     --bootstrap-server=localhost:9092 \
     --describe \
--- docker/docker-kafka-topics-list.sh
+++ docker/docker-kafka-topics-list.sh
@@ -3,10 +3,19 @@
 cd $(dirname $0)
 
 echo -e "\033[33mListing all the topics in the cluster...\033[0m"
+echo -e "\033[32mUsage:\033[0m"
+echo -e "\t$0 [BROKER]\n"
 
-echo -e "\n$ docker exec -t kpoc_broker1 kafka-topics.sh --bootstrap-server=localhost:9092 --list\n"
+BROKER="$1"
+shift
 
-docker exec -t kpoc_broker1 \
+if [ "$BROKER" == "" ]; then
+  BROKER="kpoc_broker1"
+fi
+
+echo -e "\n$ docker exec -t $BROKER kafka-topics.sh --bootstrap-server=localhost:9092 --list\n"
+
+docker exec -t $BROKER \
   kafka-topics.sh \
     --bootstrap-server=localhost:9092 \
     --list
